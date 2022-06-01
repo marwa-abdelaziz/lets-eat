5eb8c165d7772c434da350dc37ffeaca0326e573
marwa abdelaziz <eng.marwa.abdelaziz@gmail.com>
2022-06-01 22:20:41 +0200

    Add initial React App

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Business from './components/Business';
+import Error from './Error';
+import './App.css';
+
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='view' element={<Business />} /> 
+        <Route path='*' element={<Error />} />
+    </Routes>
+  </BrowserRouter>
+  );
+}
+
+export default App;
